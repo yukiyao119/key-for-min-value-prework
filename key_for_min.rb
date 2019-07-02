4b1ad9bd8b0559974a7821cfa15a87ea4b7e951b
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-02 03:41:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_key = nil 
   
   name_hash.each do |key, value|
-    if value < min || in == nil 
+    if value < min || min == nil 
       min = value
       min_key = key 
     end
